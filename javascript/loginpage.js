251c85f807934b00c3262db721ac42bcda477465
Mark Bryan <mark.bryan@uoit.net>
2018-03-23 00:48:28 -0400

    Added close button for sign up, sign in, and about sections.  Then added jquery functionality for those buttons

--- javascript/loginpage.js
+++ javascript/loginpage.js
@@ -2,24 +2,38 @@
 
 $(document).ready(function(){
 
-
   $(".signInForm").hide();
   $(".signUpForm").hide();
+  $(".aboutWindow").hide();
 
   $(".signUpToggle").click(function(){
     $(".signUpForm").slideToggle(200);
     $(".signInForm").hide();
-
+    $(".aboutWindow").hide();
   });
 
   $(".signInToggle").click(function(){
     $(".signInForm").slideToggle(200);
     $(".signUpForm").hide();
+    $(".aboutWindow").hide();
   });
 
+  $(".aboutToggle").click(function(){
+    $(".aboutWindow").slideToggle(200);
+    $(".signUpForm").hide();
+    $(".signInForm").hide();
+  });
 
+  $(".signUpCloseButton").click(function(){
+    $(".signUpForm").slideToggle();
+  });
 
+  $(".signInCloseButton").click(function(){
+    $(".signInForm").slideToggle();
+  });
 
-
+  $(".aboutCloseButton").click(function(){
+    $(".aboutWindow").slideToggle();
+  });
 
 });
